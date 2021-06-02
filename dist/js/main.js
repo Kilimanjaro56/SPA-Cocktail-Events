6f4c60041b8eeeb37a5f81f6d64cf2e6a3c4f7cf
Kilimanjaro56 <keeklypaige@gmail.com>
2021-06-02 20:56:32 +1200

    First commit, add html, css, js files and html doc setup

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+const cocktails = [
+    {
+        name:"Cosmopolitan",
+        imgId:"cosmopolitan.jpeg",
+        mainAlcohol:"Vodka",
+        ingredients:"1 1/2 oz Vodka Citron, 1 oz Cranberry juice, 1/2 oz Cointreau, 1/2 oz Fresh lime juice.",
+        served:"Straight up; without ice.",
+        garnish:"Lemon slice, Lime wedge",
+        drinkware:"Cocktail Glass",
+        category:"vodka",
+        price:8,
+    },
+    
+];
+
+function init(){
+    displayCocktails(cocktails);
+}
+
+function displayCocktails(cocktailArray){
+    let html = ``;
+    for(const cocktail of cocktailArray){
+        html += ``    }
+}
+
+
+init();
